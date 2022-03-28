cdfd5c605880f7f9b30a82d46e3241c0f463ce09
Lukas Ružauskas <nequadora@gmail.com>
2022-03-28 16:25:24 +0300

    tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import City from "./components/City";
+import InputField from "./components/InputField";
 
-test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+test("City component renders given name", () => {
+    render(
+        <City
+            name="CityName"
+            country="CountryName"
+            timezone="TimeZone"
+            image="test"
+        />
+    );
+    const linkElement = screen.getByText(/CityName/);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test("Search component renders input field", () => {
+    render(<InputField name="CityName" onChange={() => console.log("test")} />);
+    const linkElement = screen.getByRole("textbox");
     expect(linkElement).toBeInTheDocument();
 });
